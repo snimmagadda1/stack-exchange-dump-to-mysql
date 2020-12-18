3bd7f3e955b5e6fa5861e392feb04e3a8d39bf17
snimmagadda1 <saiguy@me.com>
2020-12-18 15:40:19 -0600

    Forgot login

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mvn clean package
 
 echo "Spring batch build completed"
 echo "Build & push"
-
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . -t snimmagadda/stacke-batch-mysql && docker push snimmagadda/stacke-batch-mysql
