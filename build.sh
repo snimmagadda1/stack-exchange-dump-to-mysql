ecd8ce538d6f132535a48feac3b9ac1723cf9ec6
snimmagadda1 <saiguy@me.com>
2020-12-18 17:28:18 -0600

    Fix build

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ mvn clean package
 echo "Spring batch build completed"
 echo "Build & push"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . -t snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER" && docker push snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER"
+docker build . -t snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER"
+docker tag snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER" snimmagadda/stacke-batch-mysql:latest
+docker push snimmagadda/stacke-batch-mysql:"$TRAVIS_BUILD_NUMBER" && docker push snimmagadda/stacke-batch-mysql:latest
