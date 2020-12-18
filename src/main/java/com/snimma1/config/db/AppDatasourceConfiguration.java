1c42a211ed07016283438eb1793467722cde61c7
snimmagadda1 <saiguy@me.com>
2020-12-18 16:55:58 -0600

    Add dialect props

--- src/main/java/com/snimma1/config/db/AppDatasourceConfiguration.java
+++ src/main/java/com/snimma1/config/db/AppDatasourceConfiguration.java
@@ -1,5 +1,6 @@
 package com.snimma1.config.db;
 
+import org.hibernate.dialect.MySQL5Dialect;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class AppDatasourceConfiguration {
@@ -19,27 +22,32 @@ public class AppDatasourceConfiguration {
     @Bean(name = "appDataSource")
     @ConditionalOnProperty(name = "app.datasource.url")
     public DataSource appDataSource(
-            AppDatasourceConfigurationProperties springDatasourceConfigurationProperties) {
+            AppDatasourceConfigurationProperties appDatasourceConfigurationProperties) {
         DriverManagerDataSource datasource = new DriverManagerDataSource();
-        datasource.setDriverClassName(springDatasourceConfigurationProperties.getDriverClassName());
-        datasource.setUrl(springDatasourceConfigurationProperties.getUrl());
-        datasource.setUsername(springDatasourceConfigurationProperties.getUsername());
-        datasource.setPassword(springDatasourceConfigurationProperties.getPassword());
+        datasource.setDriverClassName(appDatasourceConfigurationProperties.getDriverClassName());
+        datasource.setUrl(appDatasourceConfigurationProperties.getUrl());
+        datasource.setUsername(appDatasourceConfigurationProperties.getUsername());
+        datasource.setPassword(appDatasourceConfigurationProperties.getPassword());
         return datasource;
     }
 
     @Bean(name = "appEntityManager")
     @PersistenceContext(unitName = "appPersU")
     public LocalContainerEntityManagerFactoryBean appEntityManagerFactory(
+            AppDatasourceConfigurationProperties appDatasourceConfigurationProperties,
             @Qualifier("appDataSource") DataSource dataSource) {
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
         adapter.setGenerateDdl(true);
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", appDatasourceConfigurationProperties.getDialect());
         LocalContainerEntityManagerFactoryBean factory =
                 new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(dataSource);
+        factory.setJpaDialect(new HibernateJpaDialect());
         factory.setPackagesToScan("com.snimma1.model");
         factory.setPersistenceUnitName("appPersU");
         factory.setJpaVendorAdapter(adapter);
+        factory.setJpaProperties(properties);
         return factory;
     }
 
--- src/main/java/com/snimma1/config/db/AppDatasourceConfigurationProperties.java
+++ src/main/java/com/snimma1/config/db/AppDatasourceConfigurationProperties.java
@@ -12,4 +12,5 @@ public class AppDatasourceConfigurationProperties {
     private String url;
     private String username;
     private String password;
+    private String dialect;
 }
