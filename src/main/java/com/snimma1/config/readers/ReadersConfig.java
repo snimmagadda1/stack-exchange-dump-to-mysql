323f9975fd93a94932acaec4f0276fb726b8db91
Sai Nimmagadda <saiguy@Sais-Mac-mini.attlocal.net>
2022-01-30 17:07:18 -0500

    Update/config defaults

--- src/main/java/com/snimma1/config/readers/ReadersConfig.java
+++ src/main/java/com/snimma1/config/readers/ReadersConfig.java
@@ -17,204 +17,158 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 @PropertySource("classpath:application.yaml")
 public class ReadersConfig {
 
-    @Value("${import.file.dir}")
-    private String dir;
+  @Value("${import.file.dir}")
+  private String dir;
 
-    @Value("${import.file.posts}")
-    private String postsFile;
+  @Value("${import.file.posts}")
+  private String postsFile;
 
-    @Value("${import.file.comments}")
-    private String commentsFile;
+  @Value("${import.file.comments}")
+  private String commentsFile;
 
-    @Value("${import.file.badges}")
-    private String badgesFile;
+  @Value("${import.file.badges}")
+  private String badgesFile;
 
-    @Value("${import.file.postHistory}")
-    private String postHistoryFile;
+  @Value("${import.file.postHistory}")
+  private String postHistoryFile;
 
-    @Value("${import.file.users}")
-    private String usersFile;
+  @Value("${import.file.users}")
+  private String usersFile;
 
-    @Value("${import.file.votes}")
-    private String votesFile;
+  @Value("${import.file.votes}")
+  private String votesFile;
 
-    @Bean
-    public ItemReader multiPostsReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + postsFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
-        Resource fileSystem = new FileSystemResource(dir + "/" + postsFile);
-        System.out.println("READING FILES AT THE DIR " + dir + postsFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(postsReader());
-        return reader;
-    }
+  @Bean
+  public ItemReader multiPostsReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + postsFile);
+    System.out.println("READING FILES AT THE DIR " + dir + postsFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(postsReader());
+    return reader;
+  }
 
-    @Bean
-    public ItemReader multiBadgesReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + badgesFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
-        Resource fileSystem = new FileSystemResource(dir + "/" + badgesFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(badgesReader());
-        return reader;
-    }
+  @Bean
+  public ItemReader multiBadgesReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + badgesFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(badgesReader());
+    return reader;
+  }
 
-    @Bean
-    public ItemReader multiPostHistoryReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + postHistoryFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
+  @Bean
+  public ItemReader multiPostHistoryReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + postHistoryFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(postHistoryReader());
+    return reader;
+  }
 
-        Resource fileSystem = new FileSystemResource(dir + "/" + postHistoryFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(postHistoryReader());
-        return reader;
-    }
+  @Bean
+  public ItemReader multiCommentsReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + commentsFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(commentsReader());
+    return reader;
+  }
 
-    @Bean
-    public ItemReader multiCommentsReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + commentsFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
+  @Bean
+  public ItemReader multiUsersReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + usersFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(usersReader());
+    return reader;
+  }
 
-        Resource fileSystem = new FileSystemResource(dir + "/" + commentsFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(commentsReader());
-        return reader;
-    }
+  @Bean
+  public ItemReader multiVotesReader() {
+    Resource[] resources = null;
+    Resource fileSystem = new FileSystemResource(dir + "/" + votesFile);
+    resources = new Resource[] {fileSystem};
+    MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
+    reader.setResources(resources);
+    reader.setDelegate(votesReader());
+    return reader;
+  }
 
-    @Bean
-    public ItemReader multiUsersReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + usersFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
+  //  ******  Singleton readers ******
+  @Bean
+  public StaxEventItemReader postsReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(Post.class);
+    return new StaxEventItemReaderBuilder<Post>()
+        .name("postReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 
-        Resource fileSystem = new FileSystemResource(dir + "/" + usersFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(usersReader());
-        return reader;
-    }
+  @Bean
+  public StaxEventItemReader commentsReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(Comment.class);
+    return new StaxEventItemReaderBuilder<Comment>()
+        .name("commentReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 
-    @Bean
-    public ItemReader multiVotesReader() {
-        Resource[] resources = null;
-        //        ResourcePatternResolver patternResolver = new
-        // PathMatchingResourcePatternResolver();
-        //        try {
-        //            resources = patternResolver.getResources(dir + votesFile);
-        //        } catch (IOException e) {
-        //            e.printStackTrace();
-        //        }
+  @Bean
+  public StaxEventItemReader badgesReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(Badge.class);
+    return new StaxEventItemReaderBuilder<Badge>()
+        .name("badgeReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 
-        Resource fileSystem = new FileSystemResource(dir + "/" + votesFile);
-        resources = new Resource[] {fileSystem};
-        MultiResourceItemReader<String> reader = new MultiResourceItemReader<>();
-        reader.setResources(resources);
-        reader.setDelegate(votesReader());
-        return reader;
-    }
+  @Bean
+  public StaxEventItemReader postHistoryReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(PostHistory.class);
+    return new StaxEventItemReaderBuilder<PostHistory>()
+        .name("postHistoryReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 
-    //  ******  Singleton readers ******
-    @Bean
-    public StaxEventItemReader postsReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(Post.class);
-        return new StaxEventItemReaderBuilder<Post>()
-                .name("postReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
+  @Bean
+  public StaxEventItemReader usersReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(User.class);
+    return new StaxEventItemReaderBuilder<User>()
+        .name("usersReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 
-    @Bean
-    public StaxEventItemReader commentsReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(Comment.class);
-        return new StaxEventItemReaderBuilder<Comment>()
-                .name("commentReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
-
-    @Bean
-    public StaxEventItemReader badgesReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(Badge.class);
-        return new StaxEventItemReaderBuilder<Badge>()
-                .name("badgeReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
-
-    @Bean
-    public StaxEventItemReader postHistoryReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(PostHistory.class);
-        return new StaxEventItemReaderBuilder<PostHistory>()
-                .name("postHistoryReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
-
-    @Bean
-    public StaxEventItemReader usersReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(User.class);
-        return new StaxEventItemReaderBuilder<User>()
-                .name("usersReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
-
-    @Bean
-    public StaxEventItemReader votesReader() {
-        Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
-        unmarsh.setClassesToBeBound(Vote.class);
-        return new StaxEventItemReaderBuilder<Vote>()
-                .name("votesReader")
-                .addFragmentRootElements("row")
-                .unmarshaller(unmarsh)
-                .build();
-    }
+  @Bean
+  public StaxEventItemReader votesReader() {
+    Jaxb2Marshaller unmarsh = new Jaxb2Marshaller();
+    unmarsh.setClassesToBeBound(Vote.class);
+    return new StaxEventItemReaderBuilder<Vote>()
+        .name("votesReader")
+        .addFragmentRootElements("row")
+        .unmarshaller(unmarsh)
+        .build();
+  }
 }
--- src/main/java/com/snimma1/config/writers/WritersConfig.java
+++ src/main/java/com/snimma1/config/writers/WritersConfig.java
@@ -4,7 +4,6 @@ import com.snimma1.custom.ConsoleItemWriter;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,21 +12,15 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 @Configuration
 @PropertySource("classpath:application.yaml")
 public class WritersConfig {
-    @Value("${export.file.csv}")
-    private String csvFile;
+  @Bean
+  public ConsoleItemWriter consoleItemWriter() {
+    return new ConsoleItemWriter();
+  }
 
-    @Value("${export.file.txt}")
-    private String txtFile;
-
-    @Bean
-    public ConsoleItemWriter consoleItemWriter() {
-        return new ConsoleItemWriter();
-    }
-
-    @Bean
-    public JpaItemWriter jpaItemWriter(
-            @Qualifier("appEntityManager") LocalContainerEntityManagerFactoryBean factory) {
-        JpaItemWriterBuilder builder = new JpaItemWriterBuilder();
-        return builder.entityManagerFactory(factory.getObject()).build();
-    }
+  @Bean
+  public JpaItemWriter jpaItemWriter(
+      @Qualifier("appEntityManager") LocalContainerEntityManagerFactoryBean factory) {
+    JpaItemWriterBuilder builder = new JpaItemWriterBuilder();
+    return builder.entityManagerFactory(factory.getObject()).build();
+  }
 }
